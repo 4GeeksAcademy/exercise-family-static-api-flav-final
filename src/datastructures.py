c859673f3f3892fe03e7fae0e67ec478e2d2529f
flaviomir2712-hue <flaviomir2712@gmail.com>
2026-04-17 09:40:42 +0000

    family

--- src/datastructures.py
+++ src/datastructures.py
@@ -4,11 +4,14 @@ Update this file to implement the following already declared methods:
 - delete_member: Should delete a member from the self._members list
 - get_member: Should return a member from the self._members list
 """
-
+        
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
         self._next_id = 1
+
+        
+
         self._members = [
             {
                 "id": self._generate_id(),
@@ -19,6 +22,7 @@ class FamilyStructure:
             }
         ]
 
+
     # This method generates a unique incremental ID
     def _generate_id(self):
         generated_id = self._next_id
@@ -26,20 +30,25 @@ class FamilyStructure:
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+       #[member me envian con first_name, age, lucky_numbers]
+       if "id" not in member:
+           member["id"] = self._generate_id(),
+           member["last_name"] = self.last_name
+           self._members.append(member)
+           return member
+       
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                self._members.remove(member)
+                return 
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                return member
+        return None 
 
-    # This method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
